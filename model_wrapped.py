32b4d0696a21cd20a71406cdbe49a92c30800551
ArtemKolmakov <46863989+ArtemKolmakov@users.noreply.github.com>
2020-10-06 17:59:26 +0700

    Update model_wrapped.py

--- model_wrapped.py
+++ model_wrapped.py
@@ -1,7 +1,12 @@
-import numpy as np
-import torch
+import torchvision
 from torch import nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.tensorboard import SummaryWriter
+from sklearn.metrics import average_precision_score, accuracy_score
+import torch
+from PIL import Image
+import numpy as np
+
 
 class FineTuneModel(nn.Module):
     def __init__(self, original_model, num_classes):
@@ -19,9 +24,16 @@ class FineTuneModel(nn.Module):
         y = self.classifier(f)
         return y
     
+    
 class ModelInterface:
     def __init__(self, model, device='cuda:0'):
+        # Sets up a timestamped log directory.
+        # logdir = "logs/train_data/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+        # Writer will output to ./runs/ directory by default
+        self.writer = SummaryWriter()
+        
         self.model = model.to(device)
+        self.unorm = UnNormalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
         self.device = device
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.05)
         self.criterion= nn.BCEWithLogitsLoss(reduction='mean')
@@ -32,38 +44,87 @@ class ModelInterface:
             verbose=True,
             mode='max',
         )
+        
+    def _one_hot2labels(self, one_hot):
+        return np.argmax(one_hot, axis=1)
+        
+    def _compute_accuracy(self, pred, target):
+        pred, target = pred.cpu().detach().numpy(), target.cpu().detach().numpy()
+        pred, target = self._one_hot2labels(pred), self._one_hot2labels(target)
+        return accuracy_score(target, pred)
+        
+    def _compute_map(self, pred, target):
+        pred, target = pred.cpu().detach().numpy(), target.cpu().detach().numpy()
+        return np.mean([average_precision_score(target[:, i], pred[:, i]) for i in range(len(pred[0]))])
     
     def _step(self, data_loader, is_train=False):
         losses = []
+        mAP = []
+        acc = []
+        step_num = 0
         for batch in data_loader:
-            img, labels = batch.values()
-            img, labels= (
-                img.to(self.device),
+            imgs, labels = batch.values()
+            imgs, labels= (
+                imgs.to(self.device),
                 labels.to(self.device),
             )
-            model_out = self.model.forward(img)
+
+            # Print images in TensorBoard
+            img_batch = imgs
+            for x in range(len(img_batch)):
+                img_batch[x] = self.unorm(img_batch[x])
+            self.writer.add_images('image_batch', img_batch, step_num)
+            step_num = step_num + 1
+
+            model_out = self.model.forward(imgs)
+            self._compute_accuracy(model_out, labels)
             loss = self.criterion(model_out, labels.type_as(model_out))
             losses.append(loss.item())
             if is_train:
                 loss.backward()
                 self.optimizer.step()
                 self.optimizer.zero_grad()
-        return np.mean(losses)
+            else:
+                mAP.append(self._compute_map(model_out, labels))
+                acc.append(self._compute_accuracy(model_out, labels))
+                
+        if is_train: return np.mean(losses)
+        else: return np.mean(losses), np.mean(mAP), np.mean(acc)
     
-    def train(self, train_dataloader, val_dataloader, save_folder='./checkpoints/', num_epochs=240):
-        best_loss = 100
-        for epoch in range(num_epochs):
-            self.model.train()
-            train_loss = self._step(
-                train_dataloader,
-                is_train=True,
-            )
-            with torch.no_grad():
-                val_loss = self._step(
-                    val_dataloader,
+    
+    def train(self, train_dataloader, val_dataloader, save_folder='./checkpoints/', num_epochs=3):
+        try:
+            best_loss = 100
+            for epoch in range(num_epochs):
+                self.model.train()
+                train_loss = self._step(
+                    train_dataloader,
+                    is_train=True,
                 )
-            if val_loss < best_loss:
-                best_loss = val_loss
-                torch.save(self.model, 'classifer.pth')
-            print(f'epoch: {epoch}, train_loss: {train_loss}, val_loss: {val_loss}')
-            self.scheduler.step(val_loss)
+                with torch.no_grad():
+                    val_loss, mAP, acc = self._step(
+                        val_dataloader,
+                    )
+                if val_loss < best_loss:
+                    best_loss = val_loss
+                    torch.save(self.model, 'classifer.pth')
+
+                self.writer.add_scalar('Loss/train', train_loss, epoch)
+                self.writer.add_scalar('Loss/val', val_loss, epoch)
+                self.writer.add_scalar('mAP/val', mAP, epoch)
+                self.writer.add_scalar('Accuracy/val', acc, epoch)
+
+                print(f'epoch: {epoch}, train_loss: {train_loss}, val_loss: {val_loss}, val_map: {mAP}, val_acc: {acc}')
+                self.scheduler.step(val_loss)
+        finally:
+            # Save results
+            self.writer.close()
+            
+    def predict(self, filepath, base_transforms):
+        image = Image.open(filepath).convert('RGB')
+        image = base_transforms(image)
+        image = torch.unsqueeze(image, 0)
+        pred = self.model.forward(image)
+        pred = pred.detach().numpy()
+        pred_labels = self._one_hot2labels(pred)[0]
+        return pred_labels, pred[0][pred_labels]
