25df5f3c5c52012e3d5d45eea09038b58988ee1f
ArtemKolmakov <46863989+ArtemKolmakov@users.noreply.github.com>
2020-10-06 18:01:19 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,34 +1,24 @@
 import argparse
-import torchvision
-
-from model_wrapped import FineTuneModel, ModelInterface
 from dataset import create_dataloader
-
-
-def get_params():
-    parser = argparse.ArgumentParser(description='train loop for classification net.')
-    parser.add_argument('csv_file', type=str, help='path to csv file that contains dataset info in format:\n1.jpg,0\n2.jpg,1')
-    parser.add_argument('root_dir', type=str, help='path to root dir with dataset')  
-    parser.add_argument('path_to_model', type=str, default='', help='path to pretraned model')
-    return parser.parse_args()
-    
+from model import *
 
 if __name__ == "__main__":
-    args = get_params()
-    
-    num_classes = args.num_classes
-    csv_file = args.csv_file
-    root_dir = args.root_dir
-    path_to_model = args.path_to_model
+    parser = argparse.ArgumentParser(description='train loop for classification net.')
+    parser.add_argument('csv_file', type=str, help='path to csv file that contains dataset info in format:\n1.jpg,0\n2.jpg,1')
+    parser.add_argument('root_dir', type=str, help='path to root dir with dataset')
+    parser.add_argument('num_epochs', type=int, default=4, help='number of epochs that will be trained')
+    parser.add_argument('--path_to_model', type=str, help='path to pretrained model')
+    args = parser.parse_args()
+
 
     train_loader, val_loader, num_classes = create_dataloader(
-        csv_file=csv_file,
-        root_dir=root_dir
+        csv_file=args.csv_file,
+        root_dir=args.root_dir
     )
-    if path_to_model:
-        model = torch.load(path_to_model)
+    if args.path_to_model:
+        model = torch.load(args.path_to_model)
     else:
         model = torchvision.models.resnext50_32x4d(pretrained=True)
         model= FineTuneModel(model, num_classes=num_classes)
     model = ModelInterface(model=model)
-    model.train(train_loader, val_loader)
+    model.train(train_loader, val_loader, num_epochs=args.num_epochs) 
